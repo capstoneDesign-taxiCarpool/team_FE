e247d6dd3880d52137bb5c5679a7c393defca09d
dandamdandam <glue0440@gmail.com>
2025-03-24 14:25:02 +0900

    feat: 공통 컴포넌트(버튼, 라벨)

--- app/signup.tsx
+++ app/signup.tsx
@@ -2,6 +2,8 @@ import { useRouter } from "expo-router";
 import { Text, View } from "react-native";
 
 import Button from "@/entities/common/components/button";
+import CircleButton from "@/entities/common/components/button_circle";
+import Label from "@/entities/common/components/label";
 
 export default function Signup() {
   const router = useRouter();
@@ -9,7 +11,9 @@ export default function Signup() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>회원가입 페이지</Text>
+      <Label title="회원가입" />
       <Button title="뒤로 가기" icon="house.fill" onPress={() => router.back()} />
+      <CircleButton icon="house.fill" onPress={() => router.push("/signup")} />
     </View>
   );
 }
---
+++ entities/common/components/label.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components/native";
+
+import { Colors, FontSizes } from "../util/style_var";
+import InnerShadow from "./inner_shadow";
+
+export default function Label({ title }: { title: string }) {
+  return (
+    <StyledLabel aria-label={`${title} 입력 라벨`}>
+      <InnerShadow />
+      <StyledText>{title}</StyledText>
+    </StyledLabel>
+  );
+}
+
+const StyledLabel = styled.View({
+  position: "relative",
+  backgroundColor: Colors.main,
+  borderRadius: "30px",
+});
+const StyledText = styled.Text({
+  color: Colors.white,
+  fontSize: FontSizes.medium,
+  fontWeight: "bold",
+  minWidth: "10rem",
+  textAlign: "center",
+  padding: "0.3rem 0.5rem",
+});

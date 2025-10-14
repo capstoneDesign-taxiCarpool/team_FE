2d6d18f071ad8fd41e6dc069027d2e73788a5544
Howon <cheerhow@naver.com>
2025-10-14 21:31:47 +0900

    feat : errorm 1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,6 +24,13 @@ export default function TabLayout() {
         options={{
           title: "마이페이지",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.circle" color={color} />,
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "rgb(148, 200, 230)",
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
         }}
       />
     </Tabs>
